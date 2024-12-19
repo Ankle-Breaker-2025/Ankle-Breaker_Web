5353f909f5d34046842944aeb946d1ad132bd09f
KWJ <81617589+pq5910@users.noreply.github.com>
2024-12-19 15:48:28 +0900

    feat: 헤더 완성

--- anklebreaker_web/src/App.jsx
+++ anklebreaker_web/src/App.jsx
@@ -1,11 +1,26 @@
 import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home/Home.jsx';
+import About from './components/About/About.jsx';
+import Team from './components/Team/Team.jsx';
+import Training from './components/Training/Training.jsx';
+import Store from './components/Store/Store.jsx';
+import Header from './components/Header/Header.jsx';
+import Footer from './components/Footer/Footer.jsx';
 
 function App() {
   return (
-    <>
-      <div>
-      </div>
-    </>
+    <Router>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/team" element={<Team/>}/>
+        <Route path="/training" element={<Training/>}/>
+        <Route path="/store" element={<Store/>}/>
+      </Routes>
+      <Footer/>
+    </Router>
   )
 }
 
--- anklebreaker_web/src/components/Home/Home.jsx
+++ anklebreaker_web/src/components/Home/Home.jsx
@@ -1,9 +1,11 @@
 import './styles/Home.css';
 
 function Home() {
-  <div className="home">
-
-  </div>
+  return(
+    <div className="home">
+      홈화면입니다.
+    </div>
+  )
 }
 
 export default Home;
