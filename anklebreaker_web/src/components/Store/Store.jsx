c1b4b0bd7a54a4996125108134475020082c96af
KWJ <81617589+pq5910@users.noreply.github.com>
2024-12-26 21:44:16 +0900

    feat: store 화면 완성

--- anklebreaker_web/src/components/Store/Store.jsx
+++ anklebreaker_web/src/components/Store/Store.jsx
@@ -1,9 +1,24 @@
 import './styles/Store.css';
+import uniformData from '../../data/uniform.json';
 
 function Store() {
-  <div className="store">
-
-  </div>
+  const bucketUrl = import.meta.env.VITE_BUCKET_URL;
+  return(
+    <div className="store">
+      <div className="section-title">UNIFORM</div>
+      <div className="section-description">전태풍 앵클브레이커의 유니폼입니다.</div>
+      <div className="section-description-2">유니폼 주문은 070-7757-0909로 연락 부탁드립니다.</div>
+      <div className="uniform-list">
+        {uniformData.uniforms.map((uniform, index) => (
+          <div key={index} className="uniform-card">
+            <img src={`${bucketUrl}/store/${uniform.image}`} alt="uniform-image"/>
+            <div className="uniform-name">{uniform.name}</div>
+            <div className="uniform-price">{new Intl.NumberFormat('ko-KR').format(uniform.price)}원</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default Store;
